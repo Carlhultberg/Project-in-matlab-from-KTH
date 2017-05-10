96f48b063ef2fad2bdb6c06cfe50a4209bb847e5
Carl Hultberg <carl.johan.hultberg@gmail.com>
2017-05-10 20:05:41 +0200

    Create Sekantmodell.m

---
+++ Sekantmodell.m
@@ -0,0 +1,24 @@
+func = @(kastv) varpanA(kastv) - 20;
+ 
+ x0=1.3;
+ x1=1.4;
+ TOL=1e-6;
+ h=inf;
+ k=0;
+ while (abs(h)>TOL)
+   h=((x1-x0)/(func(x1)-func(x0)))*func(x1);
+   x2=x1-h;
+   x0=x1;
+   x1=x2;
+   h
+   k=k+1; 
+   if(mod(k, 100) == 0)
+         x1
+         x2
+   end
+ end 
+x1
+x2
+
+
+%varpanA(0.7690)
