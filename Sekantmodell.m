d9cef479cb7e4ca4f32c488c620dde74eba04fc9
Carl Hultberg <carl.johan.hultberg@gmail.com>
2017-05-12 15:03:20 +0200

    Update Sekantmodell.m

--- Sekantmodell.m
+++ Sekantmodell.m
@@ -1,10 +1,13 @@
+%function from Varpan. -20 to count when it hits the ground at 20m
 func = @(kastv) varpanA(kastv) - 20;
  
- x0=1.3;
- x1=1.4;
- TOL=1e-6;
+ x0=1.3;  %starting guess
+ x1=1.4;   %starting guess
+ TOL=1e-6; % tolerans
  h=inf;
- k=0;
+ k=0; % iterations
+ 
+  %main loop to find zero values on x-axis
  while (abs(h)>TOL)
    h=((x1-x0)/(func(x1)-func(x0)))*func(x1);
    x2=x1-h;
@@ -12,6 +15,8 @@ func = @(kastv) varpanA(kastv) - 20;
    x1=x2;
    h
    k=k+1; 
+   
+   % only print every 100th iteration
    if(mod(k, 100) == 0)
          x1
          x2
