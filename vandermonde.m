1618446765e7fa2f54f596700aa983cca06d3e3a
Carl Hultberg <carl.johan.hultberg@gmail.com>
2017-05-12 14:54:11 +0200

    Update vandermonde.m

--- vandermonde.m
+++ vandermonde.m
@@ -1,6 +1,6 @@
 pv = []; %vector
 x = 0; 
-a =[-0.0727, 1.3652, 1.6948]; %coefficients from vandermonde
+a =[-0.0727, 1.3652, 1.6948]; %coefficients from interpolation
 func = @(x) a(3) + a(2)*x + a(1)*x^2;
 
 %loop which iterates and saves the y-position for every x in R from 1 to 20.
